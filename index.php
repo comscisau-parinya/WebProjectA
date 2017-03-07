4de14b9efc368967fe1220cbd5a1ecdb54d2dd85
comscisau-parinya <i3lacki3i40@gmail.com>
2017-03-07 16:14:47 +0700

    update #1 na ja

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home Page</title>
+</head>
+<body>
+<center>
+	<h1>Home Page</h1>
+	<hr>
+	<form action="singinprocess.php" method="post">
+	Username :
+	<input type="text" name="uname">
+	<br><br>
+	Passwod :
+	<input type="password" name="upassword">
+	<br><br>
+	<input type="submit" value="Sign In">
+	<input type="reset" value="Cancel">
+
+	</form>
+	<br>
+	<a href="registermember.php">ลงทเบียนเรียน</a>
+</center>
+
+</body>
+</html>
